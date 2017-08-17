c285b6ef9aa99a18e807e445bbc0902dea358040
Clifford Chapman <clifford.chapman@ul.com>
2017-08-17 03:15:09 -0400

    Flesh out pandoc task, make it run properly.

--- pandoc-task/pandoc.js
+++ pandoc-task/pandoc.js
@@ -1,31 +1,84 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : new P(function (resolve) { resolve(result.value); }).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 const task = require("vsts-task-lib/task");
-function runPandoc(tool, filePath, pandocArgs) {
-    var toolPath = task.which('pandoc');
-    var atool = task.tool(toolPath).arg();
+const path = require("path");
+function getExtension(format) {
+    switch (format) {
+        case "markdown":
+        case "markdown_strict":
+        case "markdown_phpextra":
+        case "markdown_github":
+        case "markdown_mmd":
+        case "commonmark":
+            return ".md";
+        case "textile":
+            return ".textile";
+        case "rst":
+            return ".rst";
+        case "html":
+            return ".html";
+        case "docbook":
+            return ".db";
+        case "docx":
+            return ".docx";
+        case "odt":
+            return ".odt";
+        case "epub":
+            return ".epub";
+        case "opml":
+            return "opml";
+        case "org":
+            return ".org";
+        case "man":
+            return ".1";
+        case "mediawiki":
+            return ".wiki";
+        case "latex":
+            return ".tex";
+        default:
+            return ".txt";
+    }
 }
-async function run() {
-    try {
-        console.log(process.env["INPUT_SAMPLESTRING"]);
-        let tool;
-        if (process.platform == 'win32') {
-            let cmdPath = task.which('cmd');
-            tool = task.tool(cmdPath).arg('/c').arg('echo ' + task.getInput('samplestring', true));
+function run() {
+    return __awaiter(this, void 0, void 0, function* () {
+        let sourceFolder = task.getPathInput('sourceFolder', true, true);
+        let targets = task.getDelimitedInput('targetfiles', '\n', true);
+        let inputFormat = task.getInput('inputFormat', true);
+        let outputFormat = task.getInput('outputFormat', true);
+        let standaloneFormat = task.getBoolInput('standalone', false);
+        let commandLineArgs = task.getInput('commandLineOptions', false);
+        sourceFolder = path.normalize(sourceFolder);
+        let allPaths = task.find(sourceFolder);
+        let matchedPaths = task.match(allPaths, targets, sourceFolder);
+        let matchedFiles = matchedPaths.filter((itemPath) => !task.stats(itemPath).isDirectory());
+        console.log("Found " + matchedFiles.length + " files to convert");
+        try {
+            matchedFiles.forEach((file) => {
+                var newfile = path.basename(file, path.extname(file)) + getExtension;
+                var outputFile = path.dirname(file) + '/' + newfile;
+                let pandoc = task.tool(task.which('pandoc', true))
+                    .arg('--from=' + inputFormat)
+                    .arg('--to=' + outputFormat)
+                    .line(commandLineArgs)
+                    .argIf(standaloneFormat, '--standalone')
+                    .arg(file)
+                    .arg('--output=' + outputFile);
+                console.log("Converting " + file + " to " + newfile);
+                pandoc.exec();
+            });
         }
-        else {
-            let echoPath = task.which('echo');
-            tool = task.tool(echoPath).arg(task.getInput('samplestring', true));
+        catch (err) {
+            task.setResult(task.TaskResult.Failed, err.message);
         }
-        let rc1 = await tool.exec();
-        if (rc1 == 0) {
-            mod.sayHello();
-        }
-        console.log('Task done! ' + rc1);
-    }
-    catch (err) {
-        task.setResult(task.TaskResult.Failed, err.message);
-    }
+    });
 }
 run();
 //# sourceMappingURL=pandoc.js.map
--- pandoc-task/pandoc.ts
+++ pandoc-task/pandoc.ts
@@ -1,32 +1,82 @@
 import task = require('vsts-task-lib/task');
 import toolrunner = require('vsts-task-lib/toolrunner');
+import path = require('path');
 
-function runPandoc(tool: toolrunner.ToolRunner, filePath: string, pandocArgs: string) {
-	var toolPath = task.which('pandoc');
-	var atool:toolrunner.ToolRunner = task.tool(toolPath).arg(
+function getExtension(format: string) {
+	switch (format) {
+		case "markdown":
+		case "markdown_strict":
+		case "markdown_phpextra":
+		case "markdown_github":
+		case "markdown_mmd":
+		case "commonmark":
+			return ".md";
+		case "textile":
+			return ".textile";
+		case "rst":
+			return ".rst";
+		case "html":
+			return ".html";
+		case "docbook":
+			return ".db";
+		case "docx":
+			return ".docx";
+		case "odt":
+			return ".odt";
+		case "epub":
+			return ".epub";
+		case "opml":
+			return "opml";
+		case "org":
+			return ".org";
+		case "man":
+			return ".1";
+		case "mediawiki":
+			return ".wiki";
+		case "latex":
+			return ".tex";
+		default:
+			return ".txt";
+	}
 }
 
 async function run() {
+	let sourceFolder = task.getPathInput('sourceFolder', true, true);
+	let targets = task.getDelimitedInput('targetfiles', '\n', true);
+	let inputFormat = task.getInput('inputFormat', true);
+	let outputFormat = task.getInput('outputFormat', true);
+	let standaloneFormat = task.getBoolInput('standalone', false);
+	let commandLineArgs = task.getInput('commandLineOptions', false);
+
+	// normalize the source folder path. this is important for later in order to accurately
+	// determine the relative path of each found file (substring using sourceFolder.length).
+	sourceFolder = path.normalize(sourceFolder);
+
+	let allPaths: string[] = task.find(sourceFolder); // Default find options including symlinks
+	let matchedPaths: string[] = task.match(allPaths, targets, sourceFolder); // Default match options, matching the target patterns
+	let matchedFiles: string[] = matchedPaths.filter((itemPath: string) => !task.stats(itemPath).isDirectory()); // Filter out directories
+
+	console.log("Found " + matchedFiles.length + " files to convert");
+
 	try {
-		console.log(process.env["INPUT_SAMPLESTRING"]);
-		let tool: toolrunner.ToolRunner;
-		if (process.platform == 'win32') {
-			let cmdPath = task.which('cmd');
-			tool = task.tool(cmdPath).arg('/c').arg('echo ' + task.getInput('samplestring', true));
-		}
-		else {
-			let echoPath = task.which('echo');
-			tool = task.tool(echoPath).arg(task.getInput('samplestring', true));
-		}
+		matchedFiles.forEach((file: string) => {
+			// Get the file without the extension
+			var newfile = path.basename(file, path.extname(file)) + getExtension;
+			var outputFile = path.dirname(file) + '/' + newfile;
 
-		let rc1: number = await tool.exec();
 
-		// call some module which does external work
-		if (rc1 == 0) {
-			mod.sayHello();
-		}
+			let pandoc = task.tool(task.which('pandoc', true))
+				.arg('--from=' + inputFormat) // Input format
+				.arg('--to=' + outputFormat) // Output format
+				.line(commandLineArgs) // Additional arguments
+				.argIf(standaloneFormat, '--standalone')
+				.arg(file) // Input file
+				.arg('--output=' + outputFile);
 
-		console.log('Task done! ' + rc1);
+			console.log("Converting " + file + " to " + newfile);
+
+			pandoc.exec();
+		});
 	}
 	catch (err) {
 		task.setResult(task.TaskResult.Failed, err.message);
